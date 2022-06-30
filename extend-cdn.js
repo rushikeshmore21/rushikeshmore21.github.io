95995b014d89bd27f65b2eb272fac0be38fcc0db
rushikeshmore21 <96849011+rushikeshmore21@users.noreply.github.com>
2022-07-01 00:07:08 +0530

    Update extend-cdn.js

--- extend-cdn.js
+++ extend-cdn.js
@@ -20,26 +20,22 @@ observer.observe(document, {
 
 
 //Shop Observer
-    var targetSecondForShopPage = document.getElementsByClassName('category-header__container');
-
+var targetSecondForShopPage = document.getElementsByClassName('category-header__container');
 const ShopObserver = new MutationObserver((mutations, obs) => {
   
 for (const mutation of mutations) {
       if (mutation.target.classList.contains(targetSecondForShopPage)) {
-            if (targetSecondForShopPage) {
+           
             if (window.location.href.indexOf("shop") > -1) { 
-            document.getElementsByClassName('category-header__container')[0].innerHTML += '<p style="margin: .1rem 0;font-size:20px;text-transform: uppercase;">10% off 12 Bottle Cases with Code 12bottle</p>';
-            document.getElementsByClassName('category-title-row')[0].innerHTML += '<p style="margin: .4rem 0;font-size:16px;text-transform: uppercase;">10% off 12 Bottle Cases with Code 12bottle</p>';
+              document.getElementsByClassName('category-header__container')[0].innerHTML += '<p style="margin: .1rem 0;font-size:20px;text-transform: uppercase;">10% off 12 Bottle Cases with Code 12bottle</p>';
+              document.getElementsByClassName('category-title-row')[0].innerHTML += '<p style="margin: .4rem 0;font-size:16px;text-transform: uppercase;">10% off 12 Bottle Cases with Code 12bottle</p>';
             }
             obs.disconnect();
             return;
-            }
       }
 }
-  
-    
-    
-  });
+
+});
   
   ShopObserver.observe(document, {
     attributes: false,
